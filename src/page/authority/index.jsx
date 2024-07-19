739b35e8f407cc8c66864d3e4f0600d62519506a
kscold <chan6502@gmail.com>
2024-07-19 16:46:49 +0900

    프로젝트 세팅

---
+++ src/page/authority/index.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import Admin from "../../assets/Admin.png"
+import User from "../../assets/User.png"
+
+const Authority = () => {
+  return (
+    <div className="authority-container">
+      <div className="authority-h1-container">어떤 서비스를 이용하시나요?</div>
+
+      <div className="authority-select-image-container">
+        <div className=""></div>
+        <div className=""></div>
+      </div>
+    </div>
+  )
+}
+
+export default Authority
---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import React, { lazy, Suspense } from "react"
+import { createBrowserRouter } from "react-router-dom"
+import Loading from "../components/Loading"
+import Layout from "../layout"
+
+const Intro = lazy(() => import("../page/intro"))
+const Authority = lazy(() => import("../page/authority"))
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Intro />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/authority",
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Authority />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+])
+
+export default router
